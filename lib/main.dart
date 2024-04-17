29973393c523cc1a357c13a8367e760d206b8797
Franz Anthony Gabijan <89113736+praanzzzz@users.noreply.github.com>
2024-04-17 20:50:17 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 14, 14, 14),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 41, 41, 41),
+        title: const Center(child: Text('Profile', style: TextStyle(color: Colors.white),)),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _buildAvatarSection,
+              _buildDetailsSection(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+  //using variables: simplicity and performance wise
+  final _buildAvatarSection  = const Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          radius: 50,
+          backgroundImage: AssetImage('assets/images/gabijan.jpg'),
+        ),
+        SizedBox(height: 20),
+        Divider(indent: 10, endIndent: 10),
+        SizedBox(height: 20),
+      ],
+    );
+  
+
+  //using function: flexibility, can have logig, can accept parameters
+  Widget _buildDetailsSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text('NAME', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.white),),
+        const Text('FRANZ ANTHONY D. GABIJAN', style: TextStyle(color: Colors.amber),),
+        const Text('YEAR LEVEL', style: TextStyle(color: Colors.white),),
+        const Text('3', style: TextStyle(color: Colors.amber),),
+        const SizedBox(height: 20),
+        const Text('SOCIALS', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.white),),
+        _buildSocialRow(Icons.mail, 'franzgabijan55@gmail.com'),
+        _buildSocialRow(Icons.facebook, 'Franz Anthony D. Gabijan'),
+      ],
+    );
+  }
+
+  Widget _buildSocialRow(IconData icon, String text) {
+    return Row(
+      children: [
+        Icon(icon, color: Colors.white),
+        const SizedBox(width: 10.0),
+        Text(text, style: const TextStyle(color: Colors.white),),
+      ],
+    );
+  }
+}
